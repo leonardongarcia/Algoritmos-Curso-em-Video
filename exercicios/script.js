c26d1fc4479f69222ecf02a0d2585b1d041c6e33
leonardongarcia <leonprogramacao@gmail.com>
2021-01-13 15:11:01 -0300

    Exercício 1

---
+++ exercicios/script.js
@@ -0,0 +1,11 @@
+
+
+function escrever () {
+    
+    
+    var p = window.document.getElementById('txt')
+    var p1 = p.value
+    var res = window.document.getElementById('res')
+    res.innerHTML += `${p1}`
+    
+}
